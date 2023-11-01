00cf01606207485fe29cf17bc7a7801610b52c74
globalworming <andreas.worm@headissue.com>
2023-11-01 20:50:35 +0100

    experiment with increased paddle collision area to prevent ball from tunneling through

--- components/flipper/flipper.gd
+++ components/flipper/flipper.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 @export var input_action: String = "move_left"
-@export var speed: float = 100
+@export var speed: float = 200
 @export var left_flipper = true
 
 # Called when the node enters the scene tree for the first time.
@@ -14,7 +14,6 @@ func _physics_process(_delta: float) -> void:
 	if left_flipper: 
 		if (rotation_degrees < -50):
 			rotation_degrees = -50
-			
 		if (rotation_degrees > 5):
 			rotation_degrees = 5
 	else:
@@ -25,15 +24,25 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed(input_action):
 		constant_torque = 0		
-		$collision_area.scale = Vector2(1, 3)
 		add_constant_torque(100000 * speed * (-1 if left_flipper else 1))
+		#$extended_collision.disabled = false
 		pass
 	
 	if Input.is_action_just_released(input_action):
 		constant_torque = 0
-		$collision_area.scale = Vector2(1, 3)
 		add_constant_torque(100000 * speed * (1 if left_flipper else -1))
+		#$extended_collision.disabled = false
 		pass
-		
 	
-	$collision_area.scale = Vector2.ONE
+	if left_flipper: 
+		if (rotation_degrees > -30) && (rotation_degrees < -15):
+			$extended_collision.disabled = false
+		else:
+			$extended_collision.disabled = true
+	else:
+		if (rotation_degrees < 30) && (rotation_degrees > 15):
+			$extended_collision.disabled = false
+		else:
+			$extended_collision.disabled = true
+			
+		

ad644782a8edc70f6aff3c057ab89e0120c73e6d
globalworming <andreas.worm@headissue.com>
2023-11-01 15:06:02 +0100

    play, limit visual jitter on paddle

--- components/flipper/flipper.gd
+++ components/flipper/flipper.gd
@@ -11,6 +11,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
+	if left_flipper: 
+		if (rotation_degrees < -50):
+			rotation_degrees = -50
+			
+		if (rotation_degrees > 5):
+			rotation_degrees = 5
+	else:
+		if (rotation_degrees > 50):
+			rotation_degrees = 50
+		if (rotation_degrees < -5):
+			rotation_degrees = -5
+	
 	if Input.is_action_just_pressed(input_action):
 		constant_torque = 0		
 		$collision_area.scale = Vector2(1, 3)
@@ -23,4 +35,5 @@ func _physics_process(_delta: float) -> void:
 		add_constant_torque(100000 * speed * (1 if left_flipper else -1))
 		pass
 		
+	
 	$collision_area.scale = Vector2.ONE
--- test_scenes/creatures/where_balls_do_damage.gd
+++ test_scenes/creatures/where_balls_do_damage.gd
@@ -1,13 +1,7 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Statistics.damage_dealt == 20:
 		TestStatus.test_success.emit()
 	pass

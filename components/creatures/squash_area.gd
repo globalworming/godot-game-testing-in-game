16196faa8e76692bb893353317770be0b2514ec6
globalworming <andreas.worm@headissue.com>
2023-11-06 14:38:37 +0100

    paddles also squash

--- components/creatures/squash_area.gd
+++ components/creatures/squash_area.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	#print("entered", body.name)
-	if (body.is_in_group("ball")):
+	if (body.is_in_group("ball") || body.name == "paddle"):
 		var node = get_parent().get_parent()
 		node.squash()
 		pass # Replace with function body.
